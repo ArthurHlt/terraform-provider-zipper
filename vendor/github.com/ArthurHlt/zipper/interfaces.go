e4d539844c6ccd165246bbab176c8d2290f145eb
ArthurHlt <arthur.halet@orange.com>
2019-03-01 01:55:07 +0100

    use go mod and latest zipper

--- vendor/github.com/ArthurHlt/zipper/interfaces.go
+++
@@ -1,8 +0,0 @@
-package zipper
-
-type Handler interface {
-	Zip(src *Source) (zip ZipReadCloser, err error)
-	Sha1(src *Source) (sha1 string, err error)
-	Detect(src *Source) bool
-	Name() string
-}
